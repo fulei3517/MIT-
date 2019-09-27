f89a13bdd2f1d49672881851b843231a79c45b04
Eric <fulei3517@163.com>
2019-09-27 16:20:08 +0800

    2.4 分支练习题

--- 6.0001 计算机科学和Python编程导论/MITTest001.py
+++ 6.0001 计算机科学和Python编程导论/MITTest001.py
@@ -1,56 +1,36 @@
 # -*- coding:utf-8 -*-
+import random
 
 # 2.2 程序分支 练习题
 # 检查3个变量 x,y,z 输出其中最大的奇数。如果其中没有奇数，就输出一个消息进行说明
 
-x = int(input("Enter x = "))
-y = int(input("Enter y = "))
-z = int(input("Enter z = "))
+# x = int(input("Enter x = "))
+# y = int(input("Enter y = "))
+# z = int(input("Enter z = "))
 
-print("x = %s y = %s z = %s" % (x, y, z))
-print(type(z))
-# 如果 x
-# if x > y and x > z:
-#     if x % 2 != 0:
-#         print("x = %s 为最大的奇数" % x)
-#     elif y > z:
-#         if y % 2 != 0:
-#             print("y = %s 为最大的奇数" % y)
-#         elif z % 2 != 0:
-#             print("y = %s 为最大的奇数" % y)
-#         else:
-#             print("没有奇数")
-#     elif z % 2 != 0:
-#         print("y = %s 为最大的奇数" % y)
-#     else:
-#         print("没有奇数")
-# elif y > z:
-#     if y % 2 != 0:
-#         print("y = %s 为最大的奇数" % y)
-#     elif z > x and z % 2 != 0:
-#         print("z = %s 为最大的奇数" % z)
-#     elif x % 2 != 0:
-#         print("x = %s 为最大的奇数" % x)
-#     else:
-#         print("没有奇数")
-# elif z % 2 != 0:
-#     print("z = %s 为最大的奇数" % z)
-# elif y % 2 != 0:
-#     print("y = %s 为最大的奇数" % y)
-# elif x % 2 !=0:
-#     print("x = %s 为最大的奇数" % x)
-# else:
-#     print("没有奇数")
+# 1. 需要输出的结果是奇数
+# 2. 需要是最大的奇数
 
-if x > y and x > z and x % 2 != 0:
-    print("x = %s is max odd" % x)
-elif y > z and y % 2 != 0:
-    print("y = %s is max odd" % y)
-elif z % 2 != 0:
-    print("z = %s is max odd" % z)
-elif y % 2 != 0:
-    print(" = %s is max odd" % y)
-elif x % 2 != 0:
-    print("x = %s is max odd" % x)
-else:
-    print("No odd number")
+def testOdd(x, y, z):
+    print("x = %d y = %d z = %d" % (x, y, z))
+    # 首先分别判断 三个参数是不是奇数 如果不是奇数 就将它值为零
+    if x % 2 == 0:
+        x = 0
+    if y % 2 == 0:
+        y = 0
+    if z % 2 == 0:
+        z = 0
+    if x == y == z == 0:
+        print("No odd number")
+    elif x > y and x > z:
+        # x 为最大值
+        print("x = %d is max odd" % x)
+    elif y > z:
+        # y是最大值
+        print("y = %d is max odd" % y)
+    else:
+        print("z = %d is max odd" % z)
+
+
+for i in range(10):
+    testOdd(random.randint(0, 100), random.randint(0, 100), random.randint(0, 100))
